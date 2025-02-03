4469275db04a729e93de4acf79e03a6ffbb0a2c9
Alberto Redondo <alberto.redondogil@holcim.com>
2025-02-03 13:41:09 +0100

    Revert "Don't manage environment variables"

--- main.tf
+++ main.tf
@@ -68,8 +68,11 @@ resource "aws_lambda_function" "this" {
     }
   }
 
-  environment {
-    variables = {}
+  dynamic "environment" {
+    for_each = length(keys(var.environment_variables)) == 0 ? [] : [true]
+    content {
+      variables = var.environment_variables
+    }
   }
 
   dynamic "dead_letter_config" {
